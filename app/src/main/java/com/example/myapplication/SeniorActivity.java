ea2622c2a91a0ad9e88d61ee67a80f7d241444a2
nestyxo <IamNestorValerio1@yahoo.com>
2023-03-01 14:47:41 -0500

    I coded the Senior Activity and started in the activity senior page

--- app/src/main/java/com/example/myapplication/SeniorActivity.java
+++ app/src/main/java/com/example/myapplication/SeniorActivity.java
@@ -3,15 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-Button seniorBTN;
+public class SeniorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_senior);
     }
 }
