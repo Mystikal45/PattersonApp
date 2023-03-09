16782e2d67c19a3846bae60567d234714110cc94
Draggo <careymcfadden123@gmail.com>
2023-03-09 14:49:07 -0500

    finished main activity - CM

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,48 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-Button seniorBTN;
+    Button seniorBTN;
+    Button mentalBTN;
+    Button lunchBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seniorBTN = (Button) findViewById(R.id.searchBTN);
+        mentalBTN = (Button) findViewById(R.id.searchBTN);
+        lunchBTN = (Button) findViewById(R.id.searchBTN);
+
+        seniorBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SeniorActivity.class);
+
+                startActivity(myIntent);
+            }
+        });
+        mentalBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MentalActivity.class);
+
+                startActivity(myIntent);
+            }
+        });
+        lunchBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, LunchActivity);
+            }
+        });
+
     }
 }
