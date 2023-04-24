7ff261804257d704de5c2f51b1db7444e7fe7792
Gebru <1345375@bcpss.org>
2023-04-24 14:45:49 -0400

    I work on the firebase and added the button and send to firebase RG

--- app/src/main/java/com/example/myapplication/SportsActivity.java
+++ app/src/main/java/com/example/myapplication/SportsActivity.java
@@ -9,11 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SportsActivity extends AppCompatActivity {
 
     // define objects for edit text and button
     Button Sport1BTN;
     Button Sport2BTN;
+    Button sendFB;
     EditText NameET;
     TextView sportname1;
     TextView sportname2;
@@ -22,10 +26,20 @@ public class SportsActivity extends AppCompatActivity {
     String emailsend,emailsubject,emailbody;
     SportInfo sport1, sport2;
 
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports);
+
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
 
         Sport1BTN = (Button) findViewById(R.id.Sport1BTN);
         Sport2BTN = (Button) findViewById(R.id.Sport2BTN);
@@ -33,7 +47,7 @@ public class SportsActivity extends AppCompatActivity {
         sportname2= (TextView) findViewById(R.id.Sportname2);
         Coachname1= (TextView) findViewById(R.id.Coachname1);
         Coachname2= (TextView) findViewById(R.id.Coachname2);
-
+        sendFB = (Button) findViewById(R.id.SendFB);
 
 
         // Getting instance of edittext and button
@@ -49,6 +63,7 @@ public class SportsActivity extends AppCompatActivity {
 
         // attach setOnClickListener to button with Intent object define in it
         Sport1BTN.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 // define Intent object with action attribute as ACTION_SEND
@@ -86,6 +101,17 @@ public class SportsActivity extends AppCompatActivity {
                 startActivity(Intent.createChooser(intent, "Choose an Email client :"));
             }
         });
+        sendFB.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+
+                myRef.setValue(sport1);
+
+            }
+        });
+
 
 
     }
