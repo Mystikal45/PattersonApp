bbe2a49cb8db6851473beb7637c622d27fd0d030
Murray <dandremurray500@gmail.com>
2023-04-26 14:42:35 -0400

    All vote buttons go to the same screen and the text changes to that day of the week

--- app/src/main/java/com/example/myapplication/LunchActivity.java
+++ app/src/main/java/com/example/myapplication/LunchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LunchActivity extends AppCompatActivity {
     Button mondayVote;
@@ -13,6 +14,7 @@ public class LunchActivity extends AppCompatActivity {
     Button wednesdayVote;
     Button thursdayVote;
     Button fridayVote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,52 @@ public class LunchActivity extends AppCompatActivity {
         thursdayVote = (Button) findViewById(R.id.thursdayVote);
         fridayVote = (Button) findViewById(R.id.fridayVote);
 
+
         mondayVote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(LunchActivity.this, VoteActivity.class);
-            public intent putExtra(String, 5 )
+                myIntent.putExtra("dayOfWeek", "monday");
                 startActivity(myIntent);
             }
 
         });
+        tuesdayVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LunchActivity.this, VoteActivity.class);
+                myIntent.putExtra("dayOfWeek", "tuesday");
+                startActivity(myIntent);
+            }
+
+        });
+        wednesdayVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LunchActivity.this, VoteActivity.class);
+                myIntent.putExtra("dayOfWeek", "wednesday");
+                startActivity(myIntent);
+            }
+
+        });
+        thursdayVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LunchActivity.this, VoteActivity.class);
+                myIntent.putExtra("dayOfWeek", "thursday");
+                startActivity(myIntent);
+            }
+
+        });
+        fridayVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LunchActivity.this, VoteActivity.class);
+                myIntent.putExtra("dayOfWeek", "friday");
+                startActivity(myIntent);
+            }
+
+        });
+
     }
 }
